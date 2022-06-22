00048fa4aa25a12320745eec39aaba9e0c633b3c
dvrepa <d.v.repnikov@gmail.com>
2022-06-22 12:56:34 +0300

    Рефакторинг

--- src/tag/tag.module.ts
+++ src/tag/tag.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TagController } from 'src/tag/tag.controller';
-import { TagService } from './tag.service';
+import { TagService } from 'src/tag/tag.service';
 
 @Module({
   controllers: [TagController],
