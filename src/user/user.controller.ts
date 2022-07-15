71f6143e4bcbb87a094f6ad8dfeb8d69267dc579
dvrepa <d.v.repnikov@gmail.com>
2022-07-15 16:46:58 +0300

    Обработка авторизации пользователя при логине

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,6 +8,7 @@ import {
 import { UserService } from 'src/user/user.service';
 import { CreateUserDto } from 'src/user/dto/createUser.dto';
 import { UserResponseInterface } from 'src/user/types/userResponse.interface';
+import { LoginUserDto } from 'src/user/dto/loginUser.dto';
 
 @Controller()
 export class UserController {
@@ -19,6 +20,16 @@ export class UserController {
     @Body('user') createUserDto: CreateUserDto,
   ): Promise<UserResponseInterface> {
     const user = await this.userService.createUser(createUserDto);
+
+    return this.userService.buildUserResponse(user);
+  }
+
+  @Post('users/login')
+  @UsePipes(new ValidationPipe())
+  async loginUser(
+    @Body('user') loginUserDto: LoginUserDto,
+  ): Promise<UserResponseInterface> {
+    const user = await this.userService.login(loginUserDto);
 
     return this.userService.buildUserResponse(user);
   }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,6 +6,8 @@ import { Repository } from 'typeorm';
 import { sign } from 'jsonwebtoken';
 import { JWT_SECRET } from 'src/environment/config';
 import { UserResponseInterface } from 'src/user/types/userResponse.interface';
+import { LoginUserDto } from 'src/user/dto/loginUser.dto';
+import { compare } from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -15,15 +17,15 @@ export class UserService {
   ) {}
 
   async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
-    const emailIsTaken = await this.userRepository.findOne({
+    const userByEmail = await this.userRepository.findOne({
       email: createUserDto.email,
     });
 
-    const usernameIsTaken = await this.userRepository.findOne({
+    const userByUsername = await this.userRepository.findOne({
       username: createUserDto.username,
     });
 
-    if (emailIsTaken || usernameIsTaken) {
+    if (userByEmail || userByUsername) {
       throw new HttpException(
         'Email or username are taken',
         HttpStatus.UNPROCESSABLE_ENTITY,
@@ -35,6 +37,42 @@ export class UserService {
     Object.assign(newUser, createUserDto);
 
     return this.userRepository.save(newUser);
+  }
+
+  async login(loginUserDto: LoginUserDto): Promise<UserEntity> {
+    /* Перечисляю все поля, потому-что поле password защищен при выборе сущности, и чтобы его взять, придется перечислить все поля вместе с ним
+       Так как поле пароля нужно только в этом методе, и нигде больше, то это оправдано */
+    const userByEmail = await this.userRepository.findOne(
+      {
+        email: loginUserDto.email,
+      },
+      {
+        select: ['id', 'email', 'username', 'bio', 'image', 'password'],
+      },
+    );
+
+    if (!userByEmail) {
+      throw new HttpException(
+        'Credentials are not valid',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
+    const isPasswordCorrect = await compare(
+      loginUserDto.password,
+      userByEmail.password,
+    );
+
+    if (!isPasswordCorrect) {
+      throw new HttpException(
+        'Credentials are not valid',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
+    delete userByEmail.password;
+
+    return userByEmail;
   }
 
   buildUserResponse(user: UserEntity): UserResponseInterface {
